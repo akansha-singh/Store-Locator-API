702f79099e42284312f0ec2766cf5142f05847e9
akansha-singh <singhakansha281@gmail.com>
2020-04-26 00:47:09 +0530

    Fetch Stores Function

---
+++ models/Store.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const StoreSchema = new mongoose.Schema({
+    storeId: {
+        type: String,
+        required: [true, 'Please add a store ID'],
+        unique: true,
+        trim: true,
+        maxlength: [10, 'Store ID must be less than 10 chars']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+
+    location: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        },
+        formattedAddress: String
+    },
+    createdAt: {
+       type: Data,
+       default: Data.now
+    }
+});
+
+module.exports = mongoose.model('Store', StoreSchema);
