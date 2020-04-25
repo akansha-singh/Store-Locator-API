0570cada2a366f9ee2912795d9fc45b6aa34cb1b
akansha-singh <singhakansha281@gmail.com>
2020-04-25 19:58:59 +0530

    routes

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+// Body parser
+app.use(express.json);
+
+// Enable cors
+app.use(cors());
+
+// Routes
+app.use('api/v1/stores',require('./routes/stores'));
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
