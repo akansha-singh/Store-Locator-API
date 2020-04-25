044dcef78449ca68bd9b9b9955acd8e48551e0ec
akansha-singh <singhakansha281@gmail.com>
2020-04-25 23:18:39 +0530

    Connect to Database

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
+
 
 const app = express();
 
