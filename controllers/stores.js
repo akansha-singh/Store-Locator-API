746520828148d16f4efe5966ec4df4d340aa22a4
akansha-singh <singhakansha281@gmail.com>
2020-04-25 20:22:30 +0530

    Stores Controller

---
+++ controllers/stores.js
@@ -0,0 +1,7 @@
+// @desc Get all stores
+// @route GET /api/v1/stores
+// @access Public 
+
+exports.getStores = (req, res, next) => {
+    res.send('Hello');
+}
--- routes/stores.js
+++ routes/stores.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const {getStores} = require('../controllers/stores');
+
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.send("Hello");
-});
+router.route('/').get(getStores);
 
 module.exports = router;
