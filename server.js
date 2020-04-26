95a8eb87f6324cc191c7198bbeb99b6412797bdb
akansha-singh <singhakansha281@gmail.com>
2020-04-26 18:29:48 +0530

    Created static folder For Frontend

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(express.json());
 // Enable cors
 app.use(cors());
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/v1/stores', require('./routes/stores'));
