b829bb7e681b086d999c1bafcca528c9f56ad6b9
akansha-singh <singhakansha281@gmail.com>
2020-04-26 17:54:02 +0530

    bugs fixed

--- server.js
+++ server.js
@@ -5,24 +5,26 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 // load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDB();
 
-
 const app = express();
 
 // Body parser
-app.use(express.json);
+app.use(express.json());
 
 // Enable cors
 app.use(cors());
 
+
 // Routes
-app.use('api/v1/stores',require('./routes/stores'));
+app.use('/api/v1/stores', require('./routes/stores'));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
